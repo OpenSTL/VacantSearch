1fd0b8086b95c7831c3a7248820d73e7593e5515
nicostran <nstranquist@gmail.com>
2019-08-31 17:53:38 -0500

    Revert "add results detail view, accordian"
    This reverts commit 3796f82a039691ceea5b7bf3d6e7655dabb891d5.

--- sidebar-03/mapbox.js
+++ sidebar-03/mapbox.js
@@ -1,12 +1,12 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoibXJhbzIiLCJhIjoiY2ppZmFoazE5MGZiMjNwcXBtb3gyenk4cyJ9.nmFZofR-kOqj8yYfaJc2XQ';
-const map = new mapboxgl.Map({
+var map = new mapboxgl.Map({
   container: 'map', // container id
   style: 'mapbox://styles/mrao2/cjzuiuexr09mn1crs8tz35s3k', // stylesheet location
   center: [-90.199402, 38.627003], // starting position [lng, lat]
   zoom: 12 // starting zoom
 });
 
-const geocoder = new MapboxGeocoder({
+var geocoder = new MapboxGeocoder({
   accessToken: mapboxgl.accessToken,
 
   // limit results to Australia
@@ -16,8 +16,8 @@ const geocoder = new MapboxGeocoder({
   bbox: [-90.55234, 38.383975, -90.13074, 39.046325],
 
 });
-// add geocoder to DOM
-document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+
+map.addControl(geocoder);
 
 map.on('load', function () {
 
@@ -56,26 +56,49 @@ map.on('load', function () {
       "circle-color": "#007cbf"
     }
   });
+  
 
-
-
+  
   geocoder.on('result', function (ev) {
     map.getSource('single-point').setData(ev.result.geometry);
   });
 
 });
 
-function highLightParcels(layer, handles) {
-  if (!Array.isArray(handles)) {
-    handles = [];
+map.on('click', function(e) {
+
+  console.log(e);
+
+  var features = map.queryRenderedFeatures(e.point, {
+      layers: ['parcel-highlights']
+  });
+
+  console.log(features);
+
+  if (!features.length) {
+      return;
   }
 
-  if (!handles.length) {
-    map.setFilter(layer, ['all', ['match', ['get', 'HANDLE'], ['x'], true, true]]);
-    map.setLayoutProperty('parcel-highlights', 'visibility', 'none');
-  } else {
-    map.setFilter(layer, ['all', ['match', ['get', 'HANDLE'], handles, true, false]]);
+  features.forEach(function(f){
+      if(f.hasOwnProperty('properties') && f.properties.hasOwnProperty('HANDLE')){
+          console.log(f.properties.HANDLE);
+      }
+  });
 
-    map.setLayoutProperty('parcel-highlights', 'visibility', 'visible');
-  }
-}
+});
+
+function highLightParcels(layer, handles)
+    {
+        if(!Array.isArray(handles)){
+            handles = [];
+        }
+
+        if(!handles.length){
+            map.setFilter(layer, ['all', ['match', ['get', 'HANDLE'], ['x'], true, true]]);
+            map.setLayoutProperty('parcel-highlights', 'visibility', 'none');
+        } else {
+            map.setFilter(layer, ['all', ['match', ['get', 'HANDLE'], handles, true, false]]);
+
+            map.setLayoutProperty('parcel-highlights', 'visibility', 'visible');
+        }
+    }
