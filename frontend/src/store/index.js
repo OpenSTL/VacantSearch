5601107df9f20dfb8f6b667cfb1d2c829ce6df80
David Wilcox <mrpetrocket@gmail.com>
2019-11-19 08:33:49 -0600

    starting to add react-redux-scroll

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,12 +1,22 @@
 import { createStore, applyMiddleware } from 'redux';
+import { createScrollMiddleware } from 'react-redux-scroll';
 import logger from 'redux-logger';
 import promiseMiddleware from 'redux-promise-middleware';
 
 import rootReducer from '../reducers';
 
+const getMiddleware = () => {
+  const scrollMiddleware = createScrollMiddleware();
+  return [
+    logger,
+    promiseMiddleware,
+    scrollMiddleware,
+  ];
+}
+
 const store = createStore(
   rootReducer,
-  applyMiddleware(logger, promiseMiddleware)
+  applyMiddleware(...getMiddleware()),
 );
 
 export default store;
