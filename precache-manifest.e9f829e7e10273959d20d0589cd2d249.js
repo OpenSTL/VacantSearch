319dcc00924ad6cbb3a602b610add9a8d27a5416
Karen Wang <jigglepuff@users.noreply.github.com>
2019-12-03 20:42:31 -0600

    Updates

---
+++ precache-manifest.e9f829e7e10273959d20d0589cd2d249.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d52f91db848bd1e2753ec05df7c3e2ee",
+    "url": "/VacantSearch/index.html"
+  },
+  {
+    "revision": "3d618f55ff453f69adcb",
+    "url": "/VacantSearch/static/css/main.38170318.chunk.css"
+  },
+  {
+    "revision": "a1cbfa530a0c21797c54",
+    "url": "/VacantSearch/static/js/2.dc955721.chunk.js"
+  },
+  {
+    "revision": "3d618f55ff453f69adcb",
+    "url": "/VacantSearch/static/js/main.a8accba1.chunk.js"
+  },
+  {
+    "revision": "d0eac76f0775ed35c260",
+    "url": "/VacantSearch/static/js/runtime-main.71ee0369.js"
+  }
+]);
